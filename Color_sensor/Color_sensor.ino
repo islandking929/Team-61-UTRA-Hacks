5f79f22a8db5990223831a99522725a92105e812
James-BTTF6 <upload.zzy@gmail.com>
2026-01-31 13:21:18 -0500

    Calibration data

--- Color_sensor/Color_sensor.ino
+++ Color_sensor/Color_sensor.ino
@@ -57,4 +57,16 @@ void loop() {
   Serial.println("  ");
   delay(100);
 
+//pure black on the target reads around 340 red, 310 green, 290 blue
+//pure black on the straight ramp reads 280 red, 260 green and 240 blue
+//dark blue on the target reads 230 red, 130 green and 70-79 blue
+//red on the target reads 60 red, 167 green, and 144 blue
+//green on the target reads 136 red, 77 green and 106 blue
+//light blue (Although there is only a small fraction of the blue ring) reads 180 red, 100 green and 60 blue
+//pure white on the boards reads 27 red, 28 green and 25 blue
+//path red reads 40-50 red, 160 green and 130-140 blue
+//Path black reads 210 red, 240-250 green, and 230 blue
+//path green reads 150 red, 80 green and 133 blue
+//when measuring the blue on the path, it reads 180-190 red, 130 green and 85 blue
+
 }
